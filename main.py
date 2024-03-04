36424a35e6dc1e7e64c99edeebbd790b2c483893
JinnFighter <jinnfighterwork@yandex.ru>
2024-03-05 00:07:35 +0300

    add: Kivy entry point

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class KindredMaker(Widget):
+    pass
+
+
+class KindredMakerApp(App):
+    def build(self):
+        return KindredMaker()
+
+
 if __name__ == '__main__':
-    print('Hello, world!')
+    KindredMakerApp().run()
